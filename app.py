4a20c33462f00dc88b685c7a646f2679a741ad56
Joel Malinao <joe.malski@gmail.com>
2020-04-23 21:35:51 +0800

    add app.py, game loop

---
+++ app.py
@@ -0,0 +1,27 @@
+# Filename: app.py
+# Description: main module for the game.
+# Date: April 23, 2020
+
+import pygame
+
+# initialize pygame module
+pygame.init()
+
+# title and icon
+pygame.display.set_caption('Save the Patient!')
+icon = pygame.image.load('assets/images/sprites/coronavirus_32.png')
+pygame.display.set_icon(icon)
+
+# create the screen
+screen = pygame.display.set_mode((800, 600))
+
+# game loop
+game_loop_flag = True
+
+while game_loop_flag:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_loop_flag = False
+
+    screen.fill((0, 0, 0))
+    pygame.display.update()
